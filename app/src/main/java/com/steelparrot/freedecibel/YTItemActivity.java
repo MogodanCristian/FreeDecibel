da6768ede7ef4fea132a5974f45cb16db9364033
FLORIN-GABRIEL CABAUA <79976336+CabauaFlorin@users.noreply.github.com>
2021-10-28 23:40:37 +0300

    [BIG UPDATE] TabLayout MP3/MP4 Fragments added

--- app/src/main/java/com/steelparrot/freedecibel/YTItemActivity.java
+++ app/src/main/java/com/steelparrot/freedecibel/YTItemActivity.java
@@ -1,33 +1,35 @@
 package com.steelparrot.freedecibel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
 import com.jakewharton.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 import com.steelparrot.freedecibel.adapter.CustomAdapter;
+import com.steelparrot.freedecibel.adapter.FragmentAdapter;
 
 public class YTItemActivity extends AppCompatActivity {
-
-//    Intent intent = new Intent(mContext, YTItemActivity.class);
-//                intent.putExtra("url", currItem.getM_url());
-//                intent.putExtra("title", currItem.getM_title());
-//                intent.putExtra("thumbnail", R.id.thumbnailImage);
-//                intent.putExtra("duration", currItem.getM_duration());
-//                intent.putExtra("views", currItem.getM_views());
-//                intent.putExtra("uploader", currItem.getM_uploader());
-//                intent.putExtra("time_upload", currItem.getM_time_upload());
 
     private ImageView mImageView;
     private TextView mTitle, mUploader, mDuration, mViews, mTimeUpload;
 
     private String url, title, duration, uploader, time_upload, thumbnail;
     private int views;
+
+    TabLayout tabLayout;
+    ViewPager2 pager2;
+    FragmentAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +43,38 @@ public class YTItemActivity extends AppCompatActivity {
         mViews = findViewById(R.id.Views);
         mTimeUpload = findViewById(R.id.Time_upload);
 
+        tabLayout = findViewById(R.id.tab_layout);
+        pager2 = findViewById(R.id.view_pager);
+
+        FragmentManager fm = getSupportFragmentManager();
+        adapter = new FragmentAdapter(fm, getLifecycle());
+        pager2.setAdapter(adapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("MP3"));
+        tabLayout.addTab(tabLayout.newTab().setText("MP4"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+        });
+
+        pager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
 
         getData();
         bindDataLayout();
-
     }
 
     private void getData() {
