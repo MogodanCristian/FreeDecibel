916be580a191b58a54bb2977565ba74c23bd06a4
MogodanCristian <79975756+MogodanCristian@users.noreply.github.com>
2021-10-28 13:20:18 +0300

    Search view added

--- app/src/main/java/com/steelparrot/freedecibel/MainActivity.java
+++ app/src/main/java/com/steelparrot/freedecibel/MainActivity.java
@@ -3,6 +3,11 @@ package com.steelparrot.freedecibel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nav_menu,menu);
+        MenuItem searchItem=menu.findItem(R.id.nav_search);
+        SearchView searchView= (SearchView) searchItem.getActionView();
+        searchView.setQueryHint("Write keywords here...");
+        searchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchView.setMaxWidth(android.R.attr.width);
+                setItemsVisibility(menu,searchItem,false);
+            }
+        });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(getApplicationContext(),query,Toast.LENGTH_SHORT).show();
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+    private void setItemsVisibility(Menu menu, MenuItem exception,boolean visibility)
+    {
+        for(int i=0; i<menu.size();i++)
+        {
+            MenuItem item=menu.getItem(i);
+            if(item!=exception)
+            {
+                item.setVisible(visibility);
+            }
+        }
     }
 }
