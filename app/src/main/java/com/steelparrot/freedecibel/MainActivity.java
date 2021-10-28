7896aa8bbd855beb0493f1b2d521a3201e072088
MogodanCristian <79975756+MogodanCristian@users.noreply.github.com>
2021-10-28 14:08:53 +0300

    Connected main activity to search view

--- app/src/main/java/com/steelparrot/freedecibel/MainActivity.java
+++ app/src/main/java/com/steelparrot/freedecibel/MainActivity.java
@@ -36,61 +36,52 @@ public class MainActivity extends AppCompatActivity {
 
     private CustomAdapter mAdapter;
     ProgressDialog mProgressDialog;
-
     ActivityMainBinding binding;
+
+    private void searchYT(String query)
+    {
+        mProgressDialog = new ProgressDialog(MainActivity.this);
+        mProgressDialog.setMessage("Loading...");
+        mProgressDialog.show();
+
+//                // create handle for RetrofitInstance interface
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("query", query);
+        Call<List<YTItem>> call = service.getYTItems(map);
+        call.enqueue(new Callback<List<YTItem>>() {
+            @Override
+            public void onResponse(Call<List<YTItem>> call, Response<List<YTItem>> response) {
+                switch (response.code()) {
+                    case 200:
+                        binding.setIsData(true);
+                        mProgressDialog.dismiss();
+                        generateDataList(response.body());
+                        break;
+                    case 404:
+                        binding.setIsData(false);
+                        Toast.makeText(getApplicationContext(),"Couldn't find nothing on YT with these keywords!",Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        binding.setIsData(false);
+                        Toast.makeText(getApplicationContext(),"Server problem... Try again!",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<YTItem>> call, Throwable t) {
+                Log.d("retrofit1", t.toString());
+                mProgressDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setIsData(false);
-        binding.btnSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(binding.searchText.getText().toString().equals("")) {
-                    Toast.makeText(getApplicationContext(),"Insert your keywords first",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                mProgressDialog = new ProgressDialog(MainActivity.this);
-                mProgressDialog.setMessage("Loading...");
-                mProgressDialog.show();
-
-//                // create handle for RetrofitInstance interface
-                GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
-                HashMap<String, String> map = new HashMap<>();
-                map.put("query", binding.searchText.getText().toString());
-                Call<List<YTItem>> call = service.getYTItems(map);
-                call.enqueue(new Callback<List<YTItem>>() {
-                    @Override
-                    public void onResponse(Call<List<YTItem>> call, Response<List<YTItem>> response) {
-                        switch (response.code()) {
-                            case 200:
-                                binding.setIsData(true);
-                                mProgressDialog.dismiss();
-                                generateDataList(response.body());
-                                break;
-                            case 404:
-                                binding.setIsData(false);
-                                Toast.makeText(getApplicationContext(),"Couldn't find nothing on YT with these keywords!",Toast.LENGTH_SHORT).show();
-                                break;
-                            default:
-                                binding.setIsData(false);
-                                Toast.makeText(getApplicationContext(),"Server problem... Try again!",Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<List<YTItem>> call, Throwable t) {
-                        Log.d("retrofit1", t.toString());
-                        mProgressDialog.dismiss();
-                        Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-
-
     }
   
     @Override
@@ -110,8 +101,8 @@ public class MainActivity extends AppCompatActivity {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                Toast.makeText(getApplicationContext(),query,Toast.LENGTH_SHORT).show();
                 searchView.clearFocus();
+                searchYT(query);
                 return true;
             }
 
